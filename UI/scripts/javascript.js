95465480d9206e3fd1ab899dcc29e388def60fe8
Samuel Gathua <gawairimu@gmail.com>
2019-01-06 21:19:57 +0300

    [Finishes #162981164]Added the scripts for pop window when the user tries to delete meetup

--- UI/scripts/javascript.js
+++ UI/scripts/javascript.js
@@ -2,5 +2,16 @@ function createNew() {
     window.open( "/home/samie/Quistioner/create_meetup.html", "myWindow", "status = 1, height = 650, width = 780, resizable = 0" )
 }
 
-
-
+function deleteConfirm() {
+  var r = confirm("Please confirm that you want to delete the meetup");
+  if (r == true) {
+  	
+    alert('The record has been deleted successfully');
+    
+  } else {
+  
+  alert('The record  has Not  been deleted');
+    
+  }
+ 
+}

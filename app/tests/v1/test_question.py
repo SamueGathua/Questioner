893202bf45744b785d175719921bef775ef6727a
Samuel Gathua <gawairimu@gmail.com>
2019-01-10 01:17:58 +0300

    [Starts #163036937]Added the failing test for the post question end point

---
+++ app/tests/v1/test_question.py
@@ -0,0 +1,23 @@
+from app import create_app
+from run import app
+from flask.testing import FlaskClient
+import unittest
+import json
+
+class TestQuestions(unittest.TestCase):
+    def setUp(self):
+         app.testing = True
+         self.app = create_app()
+         self.client = self.app.test_client()
+    def create_record(self):
+        new_rec = {
+        "question": "Who discovered python?"
+        }
+        response = self.client.post('/api/v1/meetups/4/questions',
+                            data=json.dumps(new_rec),
+                            headers={"content-type": "application/json"})
+        return response
+
+    def test_question_post(self):
+        r = self.create_record()
+        self.assertEqual(r.status_code, 201)

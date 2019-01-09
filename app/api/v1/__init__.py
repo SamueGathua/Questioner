5df5e51dea6e2e4788ddef36a7990c039d405715
Samuel Gathua <gawairimu@gmail.com>
2019-01-10 02:03:41 +0300

    [Finishes #163038470]Added the resource for the confirm attendance endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,7 +6,9 @@ api = Api(version_one)
 
 from .views.meetup_views import Meetup, MeetupId
 from .views.question_views import Question
+from .views.confirm_views import ConfirmAttendance
 
 api.add_resource(Meetup, '/meetups')
 api.add_resource(MeetupId, '/meetups/<int:id>')
 api.add_resource(Question, '/meetups/<int:id>/questions')
+api.add_resource(ConfirmAttendance, '/meetups/<int:id>/confirms')

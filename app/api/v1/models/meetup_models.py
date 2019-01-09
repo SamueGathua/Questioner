0847c9e5d4edd2b068e2133a9f2ae14488ac58a2
Samuel Gathua <gawairimu@gmail.com>
2019-01-10 00:03:02 +0300

    [Starts #163037365] Added the model for get specific Meetup

--- app/api/v1/models/meetup_models.py
+++ app/api/v1/models/meetup_models.py
@@ -8,7 +8,7 @@ class MeetupRecords():
 
     def save(self, title, description, host, venue, date):
         data = {
-        "Meetupid": len(meetup_record) + 1,
+        "id": len(meetup_record) + 1,
         "postedOn":datetime.datetime.now(),
         "title": title,
         "description": description,
@@ -21,3 +21,10 @@ class MeetupRecords():
 
     def get_records(self):
         return meetup_record
+
+    def find(self, id):
+        result = False
+        for meetup in meetup_record:
+            if meetup['id'] == id:
+                return meetup
+        return result

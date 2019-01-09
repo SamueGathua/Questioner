c16a5f7b88652383db7981af2f3c425b6200eaf6
Samuel Gathua <gawairimu@gmail.com>
2019-01-09 22:10:26 +0300

    [Feature #163036684]created the post new meetup model

---
+++ app/api/v1/models/meetup_models.py
@@ -0,0 +1,20 @@
+import datetime
+
+meetup_record = []
+
+class MeetupRecords():
+    def __init__(self):
+        self.rec = meetup_record
+
+    def save(self, title, description, host, venue, date):
+        data = {
+        "Meetupid": len(meetup_record) + 1,
+        "postedOn":datetime.datetime.now(),
+        "title": title,
+        "description": description,
+        "host": host,
+        "venue": venue,
+        "date" : date
+        }
+        meetup_record.append(data)
+        return meetup_record

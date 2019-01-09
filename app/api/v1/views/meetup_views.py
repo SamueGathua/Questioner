a2fdb6e96c2e43227524112094e5bdc4c606c7f7
Samuel Gathua <gawairimu@gmail.com>
2019-01-09 22:12:17 +0300

    [Feature #163036684]added the post meetup view

---
+++ app/api/v1/views/meetup_views.py
@@ -0,0 +1,19 @@
+from flask_restful import Resource
+from flask import jsonify, make_response, request
+
+from ..models.meetup_models import MeetupRecords
+
+class Meetup(Resource, MeetupRecords):
+    def __init__(self):
+        self.records = MeetupRecords()
+
+    def post(self):
+         data = request.get_json()
+         title = data['title']
+         description = data['description']
+         host = data['host']
+         venue = data['venue']
+         date = data['date']
+         res = self.records.save(title, description, host, venue, date)
+         return make_response(jsonify({"A new record with the following data": res}), 201)
+   

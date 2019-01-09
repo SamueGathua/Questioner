6e21f8475eb488abe7f69ac2c40ddaff4f8c23fc
Samuel Gathua <gawairimu@gmail.com>
2019-01-10 00:04:10 +0300

    [Feature #163037365] Added the view for get specific Meetup

--- app/api/v1/views/meetup_views.py
+++ app/api/v1/views/meetup_views.py
@@ -15,8 +15,19 @@ class Meetup(Resource, MeetupRecords):
          venue = data['venue']
          date = data['date']
          res = self.records.save(title, description, host, venue, date)
-         return make_response(jsonify({"A new record with the following data": res}), 201)
+         return make_response(jsonify({"A new record with the following data has been added": res}), 201)
 
     def get(self):
         res = self.records.get_records()
         return make_response(jsonify({"The available records are": res}), 200)
+
+class MeetupId(Resource, MeetupRecords):
+    def __init__(self):
+        self.records = MeetupRecords()
+
+    def get(self, id):
+        rec = self.records.find(id)
+        if rec:
+            return make_response(jsonify({"My new records are": rec}), 200)
+        else:
+            return make_response(jsonify({"Msg": "Meetup record not found"}), 404)

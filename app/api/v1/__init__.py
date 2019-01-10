17b10bfd2844ea31351ac9ff2c53e4ff9f8af88e
Samuel Gathua <gawairimu@gmail.com>
2019-01-10 13:58:34 +0300

    [Finishes #163112629] Added the resource for the Signup endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -8,8 +8,10 @@ from .views.meetup_views import Meetup, MeetupId
 from .views.question_views import Question
 from .views.confirm_views import ConfirmAttendance
 from .views.voting_views import Voting
+from .views.signup_views import Signup
 
 api.add_resource(Meetup, '/meetups')
+api.add_resource(Signup, '/signup')
 api.add_resource(MeetupId, '/meetups/<int:id>')
 api.add_resource(Question, '/meetups/<int:id>/questions')
 api.add_resource(ConfirmAttendance, '/meetups/<int:id>/confirms')

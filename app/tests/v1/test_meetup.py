ea79d5b78153a1ca762586d13c55caa0cb403c05
Samuel Gathua <gawairimu@gmail.com>
2019-01-11 09:05:10 +0300

    [Bug #163140476]Incoporate the confirm attendance in the meetup testing

--- app/tests/v1/test_meetup.py
+++ app/tests/v1/test_meetup.py
@@ -30,3 +30,20 @@ class TesstMeetupEndpoints(unittest.TestCase):
          self.create_record()
          r = self.client.get("/api/v1/meetups", headers={"content-type": "application/json"})
          self.assertEqual(r.status_code, 200)
+class TestConfirm(unittest.TestCase):
+    def setUp(self):
+         app.testing = True
+         self.app = create_app()
+         self.client = self.app.test_client()
+    def create_record(self):
+        new_rec = {
+        "confirm": "Yes?"
+        }
+        response = self.client.post('/api/v1/meetups/4/confirms',
+                            data=json.dumps(new_rec),
+                            headers={"content-type": "application/json"})
+        return response
+
+    def test_confirm_post(self):
+        r = self.create_record()
+        self.assertEqual(r.status_code, 201)

7b0483976acea2fe55f0b45498e2e2e5802206f4
Samuel Gathua <gawairimu@gmail.com>
2019-01-11 09:33:23 +0300

    [Bug #163140997]combined the voting and question endpoint

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource
 from flask import jsonify, make_response, request
 
-from ..models.question_models import QuestionRecord
+from ..models.question_models import QuestionRecord, VotingRecord
+
+
 class Question(QuestionRecord, Resource):
     def __init__(self):
         self.records = QuestionRecord()
@@ -12,3 +14,15 @@ class Question(QuestionRecord, Resource):
         question = data['question']
         responce = self.records.save(meetup_id, question)
         return make_response(jsonify({"A new question record has been created with the following details": responce}), 201)
+
+
+class Voting(VotingRecord, Resource):
+    def __init__(self):
+        self.records = VotingRecord()
+
+    def post(self, id):
+        data = request.get_json()
+        question_id = id
+        vote = data['vote']
+        responce = self.records.save(question_id, vote)
+        return make_response(jsonify({"A new vote record has been created with the following details": responce}), 201)

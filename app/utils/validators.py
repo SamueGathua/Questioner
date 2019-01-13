85422c5fbac1a6d5215d852c4ecfb1df75dd5384
Samuel Gathua <gawairimu@gmail.com>
2019-01-13 16:48:47 +0300

    [Starts #163177325]Added key validations for the endpoints

---
+++ app/utils/validators.py
@@ -0,0 +1,38 @@
+class Validations():
+    def validate_meetup_keys(self, data):
+
+        try:
+            title = data['title']
+            description = data['description']
+            venue = data['venue']
+            date = data['date']
+
+        except KeyError:
+            data = False
+        return data
+
+    def validate_question_keys(self, data):
+
+        try:
+            question = data['question']
+
+        except KeyError:
+            data = False
+        return data
+
+    def validate_confirm_attendance_keys(self, data):
+
+        try:
+            confirm = data['confirm']
+
+        except KeyError:
+            data = False
+        return data
+    def validate_voting_keys(self, data):
+
+        try:
+            vote = data['vote']
+
+        except KeyError:
+            data = False
+        return data

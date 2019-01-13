478bcba69900311dbe52425c311afd828bdd18ec
Samuel Gathua <gawairimu@gmail.com>
2019-01-13 16:51:59 +0300

    [Finishes #163177325]Added key validations for the question endpoints

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -2,18 +2,24 @@ from flask_restful import Resource
 from flask import jsonify, make_response, request
 
 from ..models.question_models import Records
+from ....utils.validators import Validations
 
 
 class Question(Records, Resource):
     def __init__(self):
+        self.validate = Validations()
         self.records = Records()
 
     def post(self, id):
         data = request.get_json()
-        meetup_id = id
-        question = data['question']
-        responce = self.records.save(meetup_id, question)
-        return make_response(jsonify({"A new question record has been created with the following details": responce}), 201)
+        data_valid = self.validate.validate_question_keys(data)
+        if data_valid:
+            meetup_id = id
+            question = data['question']
+            responce = self.records.save(meetup_id, question)
+            return make_response(jsonify({"A new question record has been created with the following details": responce}), 201)
+        else:
+             return make_response(jsonify({"Error":"Unrecognised field"}), 400)
 
 
 class Voting(Records, Resource):

6fc4bc63f8a4ad17b68972882744a7537e38dc00
Samuel Gathua <gawairimu@gmail.com>
2019-01-15 00:28:36 +0300

    [Feature #163208227]Created the model function for the patch vote endpoint

--- app/api/v1/models/question_models.py
+++ app/api/v1/models/question_models.py
@@ -5,7 +5,7 @@ voting_record =[]
 class QuestionRecords():
     def __init__(self):
         self.rec  = question_record
-        self.rec = voting_record
+
 
     def save(self, meetup_id, question):
         data = {
@@ -17,13 +17,48 @@ class QuestionRecords():
         question_record.append(data)
         return question_record
 
-class VotingRecords():
-    def save(self, question_id, vote):
+class VotingRecord():
+    def __init__(self):
+        self.rec = voting_record
+
+    def locate(self, id):
+        res = None
+        for item in self.rec:
+            if item['question_id'] == id:
+                res = item
+        return res
+
+    def save(self, id, vote):
+
         data = {
-        "vote_id": len(voting_record) + 1,
-        "question_id": question_id,
-        "voted-on": datetime.datetime.now(),
-        "vote": vote
+            "question_id" : id,
+            "upvotes" : 0,
+            "downvotes" : 0,
+            "voted_on" : datetime.datetime.now()
         }
-        voting_record.append(data)
-        return voting_record
+        #Executed when the vote parsed is True
+
+        if vote:
+            data['upvotes'] = data['upvotes'] + 1
+
+        #Executed when the vote parsed is True
+
+        else:
+            data['downvotes'] = data['downvotes'] + 1
+        self.rec.append(data)
+        return self.rec
+
+    def vote(self, id, vote):
+        rec = self.locate(id)
+
+        #checks if the voting record does exist
+
+        if rec is not None:
+            if vote == True:
+                rec['upvotes'] = rec['upvotes'] + 1
+            else:
+                rec['downvotes'] = rec['downvotes'] -1
+            return rec
+        else: ### record does not exists
+            res = self.save(id, vote)
+        return res

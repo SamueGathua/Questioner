28d7d2fc6cb867f25e96d907d44fb3c02ab98722
Samuel Gathua <gawairimu@gmail.com>
2019-01-15 14:51:18 +0300

    [Starts #163220857]Added validation for the post question endpoint using the request parsing

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -1,30 +1,31 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import jsonify, make_response, request
-
 from ..models.question_models import QuestionRecords, VotingRecord
-from ....utils.validators import Validations
-
 
 class Question(QuestionRecords, Resource):
     def __init__(self):
-        self.validate = Validations()
+
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('question', type=str, required=True, help='Invalid key')
+        reqdata = self.parser.parse_args(strict=True)
         self.records = QuestionRecords()
 
     def post(self, id):
+        reqdata = self.parser.parse_args(strict=True)
         data = request.get_json()
-        data_valid = self.validate.validate_question_keys(data)
-        #validate the key data for the question record
-        if data_valid:
-            meetup_id = id
-            question = data['question']
-            responce = self.records.save(meetup_id, question)
-            return make_response(jsonify({"status":201,
-                                        "A new question record has been created with the following details": responce}), 201)
-        else:
-            #when an invalid key is parsed
-             return make_response(jsonify({"status":400,
-                                        "Error":"Unrecognised key"}), 400)
 
+        #Checks that data received from post question is not empty
+
+        if not reqdata['question']:
+            #if data received is empty an error message is thrown
+
+            return make_response(jsonify({"status":400,
+                                       "Error":"Qeestion field cannot be empty"}), 400)
+
+        #Executed wgen  all above checks pass
+        responce = self.records.save(id, reqdata['question'])
+        return make_response(jsonify({"status":201,
+                                    "A new question record has been created with the following details": responce}), 201)
 class Upvotes(VotingRecord, Resource):
     def __init__(self):
         self.rec = VotingRecord()

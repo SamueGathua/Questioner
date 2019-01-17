e6b9a5f4e58d4cad905c0fb467bf5c6f663b987e
Samuel Gathua <gawairimu@gmail.com>
2019-01-17 17:24:15 +0300

    [Finishes #163293370]registered the version two blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, Blueprint
 
 from .api.v1 import version_one as v1
+from .api.v2 import version_two as v2
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(v1)
+    app.register_blueprint(v2)
     return app

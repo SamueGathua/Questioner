7ad8c11c7505ad45d1814f5f250822e1415d9dda
Samuel Gathua <gawairimu@gmail.com>
2019-01-19 01:51:05 +0300

    [Chore #163339138]Added the user table creation module

---
+++ app/utils/tables.py
@@ -0,0 +1,14 @@
+class Tables():
+    """ Class to create tables """
+    def table_queries(self):
+        """ Create user registration table"""
+        users = """CREATE TABLE IF NOT EXISTS users
+        (
+        Id  SERIAL PRIMARY KEY,
+        FirstName char(50) NOT NULL,
+        LastName char(50) NOT NULL,
+        Email char(50) NOT NULL,
+        Password char(30) NOT NULL
+        )"""
+        self.query = [users]
+        return self.query

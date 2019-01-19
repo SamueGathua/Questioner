3b7c5c998a457b87f5c5d62d19cf3be5a5844931
Samuel Gathua <gawairimu@gmail.com>
2019-01-19 10:42:08 +0300

    modified db connect

--- app/utils/dbconnect.py
+++ app/utils/dbconnect.py
@@ -1,11 +1,11 @@
 import os
 import psycopg2
 from tables import Tables
-
+URL = "dbname=questionerdb host=localhost port=5432 user=postgres password='root'"
 DB_URL = os.getenv('DATABASE_URL')
 
 def init_db():
-    con = psycopg2.connect(DB_URL)
+    con = psycopg2.connect(URL)
     return con
 
 def create_tables():

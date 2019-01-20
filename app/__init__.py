3bd4aaecbe721328988b1217864693741b1fb688
Samuel Gathua <gawairimu@gmail.com>
2019-01-20 22:37:40 +0300

    [Chore #163350152]initialized the creation of tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint
+from .utils.dbconnect import create_tables
 
 from .api.v1 import version_one as v1
 from .api.v2 import version_two as v2
@@ -7,4 +8,6 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(v1)
     app.register_blueprint(v2)
+    create_tables()
+
     return app

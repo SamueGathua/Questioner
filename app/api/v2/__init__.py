6461687d23dd3e1a5f0a8de456b29965478a02a3
Samuel Gathua <gawairimu@gmail.com>
2019-01-22 13:50:08 +0300

    [Feature #163382841]Added the route for user login

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -4,7 +4,8 @@ from flask import Blueprint
 version_two = Blueprint('api_v2', __name__, url_prefix='/api/v2')
 api = Api(version_two)
 
-from .views.user_views import Signup
+from .views.user_views import Signup, AuthenticateUser
 
 
 api.add_resource(Signup, '/user/signup')
+api.add_resource(AuthenticateUser, '/user/login')

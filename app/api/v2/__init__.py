9edb4cf53cb798e0b46c9ce235a4ebb5708a765e
Samuel Gathua <gawairimu@gmail.com>
2019-01-22 05:57:48 +0300

    [Feature #163365059]Added the route for register user endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -4,8 +4,7 @@ from flask import Blueprint
 version_two = Blueprint('api_v2', __name__, url_prefix='/api/v2')
 api = Api(version_two)
 
-from .views.user_views import Signup, AuthenticateUser
+from .views.user_views import Signup
 
 
 api.add_resource(Signup, '/user/signup')
-api.add_resource(AuthenticateUser, '/user/login')

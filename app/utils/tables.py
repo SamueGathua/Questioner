6a9dc9237c7da1456aed6c062539e2224ec41e8b
Samuel Gathua <gawairimu@gmail.com>
2019-01-23 00:51:11 +0300

    [Feature #163399860] Modified the table to include relation in meetups and users table

--- app/utils/tables.py
+++ app/utils/tables.py
@@ -8,28 +8,23 @@ class Tables():
         LastName char(50) NOT NULL,
         OtherName char(50) NOT NULL,
         Email char(50) NOT NULL,
-        Password char(30) NOT NULL,
+        Password char(250) NOT NULL,
         PhoneNumber char(10) NOT NULL,
         IsAdmin  BOOL ,
         RegisteredOn TIMESTAMP
         )"""
         meetups = """CREATE TABLE IF NOT EXISTS meetups
         (
-        Id  SERIAL PRIMARY KEY ,
+        Id  SERIAL NOT NULL UNIQUE ,
+        U_id INTEGER REFERENCES users(Id),
         Title char(50) NOT NULL,
         Description char(50) NOT NULL,
-        Venue char(50) NOT NULL,
-        Date char(50) NOT NULL,
-        PostedOn TIMESTAMP
-        )"""
-        questions = """CREATE TABLE IF NOT EXISTS questions
-        (
-        Id  SERIAL,
-        M_id INTEGER REFERENCES meetups(Id),
-        Question char(50) NOT NULL,
-        Votes INTEGER,
+        Venue char(32) NOT NULL,
+        Date char(32) NOT NULL,
         PostedOn TIMESTAMP,
-        PRIMARY KEY (Id, M_id)
+        Tags char(25) NOT NULL,
+        PRIMARY KEY (Id, U_id)
         )"""
-        self.query = [users,meetups,questions]
+
+        self.query = [users,meetups]
         return self.query

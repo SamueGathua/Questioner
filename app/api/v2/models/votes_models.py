a4f976b64c65cf7d773012713841ff945c338b8e
Samuel Gathua <gawairimu@gmail.com>
2019-01-24 20:37:23 +0300

    [Starts #163454178]Added the model for voting endpoint

--- app/api/v2/models/votes_models.py
+++ app/api/v2/models/votes_models.py
@@ -7,21 +7,18 @@ class VotesRecords():
         def __init__(self):
             self.db  = init_db()
 
-        def save_upvote(self,q_id,vote):
-
+        def save_upvote(self, q_id, data):
             query = "UPDATE questions  set votes = votes +1 WHERE id = '{}'".format(q_id)
-
             save = self.db
             cur = save.cursor()
             cur.execute(query)
             save.commit()
-            return vote
+            return data
 
-        def save_downvote(self,q_id,vote):
-
+        def save_downvote(self,q_id, data):
             query = "UPDATE questions  set votes = votes -1 WHERE id = '{}'".format(q_id)
             save = self.db
             cur = save.cursor()
             cur.execute(query)
             save.commit()
-            return vote
+            return data
